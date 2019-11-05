920965a3d7b6b439222d4d997aab6f6e79e1ce97
CuongDinh1001 <Dinhquoccuong1001@gmail.com>
2019-11-05 23:06:15 +0700

    add project

---
+++ public/execute.js
@@ -0,0 +1,71 @@
+var socket = io("http://localhost:3000");
+$(document).ready(function(){
+    $("#loginForm").show();
+    $("#createRoom").hide();
+    $("#chatForm").hide();
+    // click Register
+    $("#btnRegister").click(function(){
+        socket.emit("client-send-username", $("#txtUsername").val());
+    });
+    // click Log out
+    $("#btnLogout").click(function(){
+        socket.emit("user-logout");
+        $("#loginForm").show(2000);
+        $("#chatForm").hide(1000);
+    });
+    // click join chat room
+    $("#btbJoin").click(function(){
+        socket.emit("user-send-roomName", $("#txtRoomID").val());
+    });
+    // click chat
+    $("#btnSend").click(function(){
+        socket.emit("user-send-message", $("#txtMessage").val());
+        $("#txtMessage").val("");
+    });
+    // is typing...?
+    $("#txtMessage").focusin(function(){
+        socket.emit("whoIsTyping");
+    });
+    $("#txtMessage").focusout(function(){
+        socket.emit("whoIsEndTyping");
+    });
+
+});
+socket.on("server-send-reg-failed", function(){
+    alert("username has already registered");
+});
+socket.on("server-send-reg-success", function(data){
+    $("#currentUser").html(data);
+    $("#loginForm").hide(1000);
+    $("#createRoom").show(1000);
+});
+
+socket.on("server-send-userArr", function(data){
+    $("#boxContent").html("");
+    data.forEach(function(i){
+        $("#boxContent").append("<div class='user'>" + i + "</div>");
+    });
+});
+
+socket.on("server-send-join-room-success", function(data){
+    $("#currentRoom").data(data);
+    $("#createRoom").hide(1000);
+    $("#chatForm").show(1000);
+});
+
+socket.on("server-send-roomArr", function(data){
+    $("#boxListRoom").html("");
+    data.forEach(function(i){
+        $("#boxListRoom").append("<div class='room'>" + i + "</div>");
+    });
+});
+
+socket.on("server-send-message", function(data){
+    $("#listMessages").append("<div class='ms'>" + data.un + ":" + data.ct + "</div>");
+});
+socket.on("Typing", function(data){
+    $("#whoIsTyping").html(data);
+});
+socket.on("endTyping", function(){
+    $("#whoIsTyping").html("");
+});
---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require("express");
+var app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
+server.listen(3000);
+
+var userArr = [];
+var roomArr = [];
+
+io.on("connection", function(socket){
+    console.log("Someone connected: " + socket.id);
+    socket.on("client-send-username", function(data){
+        if (userArr.indexOf(data)>=0) {
+            // fail
+            socket.emit("server-send-reg-failed");
+        } else {
+            // success
+            userArr.push(data);
+            socket.emit("server-send-reg-success", data);
+            socket.username = data;
+            io.sockets.emit("server-send-userArr", userArr);
+            io.sockets.emit("server-send-roomArr", roomArr);
+        }
+    });
+
+    socket.on("user-logout", function(){
+        // remove user from list
+        userArr.splice(userArr.indexOf(socket.username), 1);
+        socket.broadcast.emit("server-send-userArr", userArr)  ;
+    });
+
+    socket.on("user-send-roomName", function(data){
+        if (roomArr.indexOf(data) < 0) {
+            roomArr.push(data);
+            socket.join(data);
+            socket.room = data;
+            socket.emit("server-send-join-room-success", data);
+            
+        }
+    });
+
+    socket.on("user-send-message", function(data){
+        io.sockets.emit("server-send-message", {un: socket.username, ct: data});
+    });
+
+    socket.on("whoIsTyping", function(){
+        var s = socket.username + " is typing...";
+        socket.broadcast.emit("Typing", s);
+    });
+    socket.on("whoIsEndTyping", function(){
+        socket.broadcast.emit("endTyping");
+    });
+});
+
+app.get("/", function(req, res){
+    res.render("homepage")
+});
