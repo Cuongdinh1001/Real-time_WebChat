512de6614c562a7797b1e461ef6842e826a136de
CuongDinh <dinhquoccuong1001@gmail.com>
2020-05-03 22:35:40 -0700

    update

--- public/execute.js
+++ public/execute.js
@@ -1,4 +1,4 @@
-var socket = io("http://localhost:3000");
+var socket = io("https://cuongdinh.herokuapp.com");
 $(document).ready(function(){
     $("#loginForm").show();
     $("#createRoom").hide();
